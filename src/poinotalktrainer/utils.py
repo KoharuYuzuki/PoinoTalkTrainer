25a651f08010936fc49137c935a5d4a204a6e7bf
KoharuYuzuki <79073521+KoharuYuzuki@users.noreply.github.com>
2024-04-03 00:35:32 +0900

    キーワードを追加

--- src/poinotalktrainer/utils.py
+++ src/poinotalktrainer/utils.py
@@ -522,7 +522,7 @@ def gen_dataset(
   NDArray | None,
   NDArray | None
 ] | None:
-  parsed = parse_label(lab_file_path, is_mono)
+  parsed = parse_label(lab_file_path, is_mono=is_mono)
   if parsed == None:
     return None
 
